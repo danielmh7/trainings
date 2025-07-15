653e176f6430d058b8bc512cc634fb90eaa0b17d
Daniel Mejia <47867104+danielmh7@users.noreply.github.com>
2025-07-15 15:50:18 -0600

    Create main function call

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Declaración de la función
+void saludar();
+
+// Definición de la función
+void saludar() {
+    printf("Hola, mundo\n");
+}
+
+int main() {
+    // Llamada a la función
+    saludar();
+    return 0;
+}
