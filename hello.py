94ceee5895481dd4fc7e3744dc11ec53ce468e44
Daniel Mejia <47867104+danielmh7@users.noreply.github.com>
2025-07-15 15:53:51 -0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Definición de la función
+def saludar():
+    print("Hola, mundo")
+
+# Función principal
+def main():
+    saludar()
+
+# Llamada al punto de entrada
+if __name__ == "__main__":
+    main()
